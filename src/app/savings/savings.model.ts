ef3cb3f3b30794bc7b3a3d0a4a29b03bc13c2e51
Tomas Tamantini <tomasbft@gmail.com>
2025-01-18 20:22:17 -0300

    Extract savings service

---
+++ src/app/savings/savings.model.ts
@@ -0,0 +1,12 @@
+export interface Savings {
+    month: string;
+    consumption: number;
+    cip: number;
+    fioB: number | null;
+    concessionRate: number;
+    concessionCost: number;
+    ascenderRate: number;
+    ascenderCost: number;
+    savings: number;
+}
+
